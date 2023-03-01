fe850f3fd53712616e78bfac00c4326d9917126a
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2023-03-01 16:45:49 -0500

    simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={process.env.PUBLIC_URL + "/MF_Doom_The_Arches_1_(headshot).jpg"} alt="MF DOOM headshot -  Wikimedia Commons" width={300} height={270}/>
+        <h1>Spotiguys Classifier</h1>
+        <p>Enter the Spotify share link for a song and see who is most likely to send it...</p>
+        <p>(Ex: https://open.spotify.com/track/0QRxtcxL31dRAeiUUuENPu?si=6ca09b3b542c4289) </p>
+        <input type="text"></input>
+        <button type="button">Submit</button>
       </header>
     </div>
   );
